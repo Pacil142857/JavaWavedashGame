cef104c69f317d53b2cbf53ec01281cdb40b31cb
Pacil142857 <7coolaustin7@gmail.com>
2022-03-30 14:24:51 -0400

    Added 3rd map

--- PremadeMap.java
+++ PremadeMap.java
@@ -20,8 +20,8 @@ public class PremadeMap {
 
     public void addBorder(Map map) {
         map.addRect(0, h - 50, w, 50);
-        map.addRect(0, 0, 25, h);
-        map.addRect(w - 25, 0, 25, h);
+        map.addRect(0, 0, 25, h - 50);
+        map.addRect(w - 25, 0, 25, h - 50);
     }
 
     // A basic map where wavedashing is not required
@@ -50,6 +50,23 @@ public class PremadeMap {
 
         map.addLightFloor(450, h - 110, 50);
         map.addLightFloor(300, h - 190, 50);
+
+        rects = map.getRects();
+        lightFloors = map.getLightFloors();
+    }
+
+    // A map that teaches the Player to jump cancel wavedashes
+    public void formMap3(Graphics g) {
+        map = new Map(6, 3);
+        addBorder(map);
+
+        map.addLightFloor(100, h - 130, 50);
+        map.addRect(150, h - 210, 100, 160);
+        map.addLightFloor(250, h - 130, 50);
+
+        map.addRect(500, h - 210, 100, 160);
+        map.addLightFloor(600, h - 130, 50);
+        map.addRect(850, h - 210, w - 775, 160);
 
         rects = map.getRects();
         lightFloors = map.getLightFloors();
